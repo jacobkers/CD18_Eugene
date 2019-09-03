bd4d859dca6572e21bc3373723cbaa8209301f18
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2019-09-03 17:01:09 +0200

    Delete A002_Kymo_Shellprogram.m

--- A002_Kymo_Shellprogram.m
+++
@@ -1,7 +0,0 @@
-function A002_Kymo_Shellprogram
-
-%build a kymograph; user-select areas of interest and calibration areas
-if 0, A005_MG_gen1; end  %UNDER CONSTRUCTION don't run please
-if 0, A007_MG_ana; end   %UNDER CONSTRUCTION don't run please
-if 1, A028_Clickit; end
-if 1, A030_BoxTracker; end
